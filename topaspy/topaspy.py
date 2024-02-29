5d40d12acf312ccc8f137b27f68e4d7f00096adf
Tom Wood <thomas.wood@stfc.ac.uk>
2024-02-29 15:41:46 +0000

    Made fname a property and wrote a very basic reload_file method

--- topaspy/topaspy.py
+++ topaspy/topaspy.py
@@ -1,3 +1,21 @@
 class Input:
     def __init__(self, fname):
-        self.fname = fname
+        self._fname = fname
+        self.reload_file()
+    
+    @property
+    def fname(self):
+        return self._fname
+    
+    @fname.setter
+    def fname(self, val):
+        self._fname = val
+        self.reload_file()
+    
+    def reload_file(self):
+        with open(self._fname, 'r') as f:
+            self.raw_string = f.read()
+        self.parse_raw_string(self.raw_string)
+    
+    def parse_raw_string(self, raw_string):
+        pass
