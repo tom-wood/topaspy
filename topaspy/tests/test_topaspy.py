984fe11395695ce85b2d7fd72cd34cb4cce303ce
Tom Wood <thomas.wood@stfc.ac.uk>
2024-03-04 15:55:13 +0000

    Methods to remove line comments and block comments

--- topaspy/tests/test_topaspy.py
+++ topaspy/tests/test_topaspy.py
@@ -15,3 +15,19 @@ class TestInput:
         assert self.inst.fname.split('/')[-1] == "test.inp"
         with pytest.raises(FileNotFoundError):
             self.inst.fname = "nonsense.inp"
+    
+    def test_remove_line_comment(self):
+        s0 = "xdd'another comment"
+        s1 = "'"
+        s2 = "xdd"
+        s3 = "xdd'comment'and another"
+        assert self.inst.remove_line_comment(s0) == "xdd"
+        assert self.inst.remove_line_comment(s1) == ""
+        assert self.inst.remove_line_comment(s2) == "xdd"
+        assert self.inst.remove_line_comment(s3) == "xdd"
+    
+    def test_remove_comments(self):
+        s0 = "xdd'another comment\n/*\nother*/"
+        s1 = "'comment\nxdd/*other*/\n"
+        assert ''.join(self.inst.remove_comments(s0)) == "xdd"
+        assert ''.join(self.inst.remove_comments(s1)) == "xdd"
--- topaspy/topaspy.py
+++ topaspy/topaspy.py
@@ -18,4 +18,37 @@ class Input:
         self.parse_raw_string(self.raw_string)
     
     def parse_raw_string(self, raw_string):
-        pass
+        self.uncommented_string = self.remove_comments(raw_string)
+    
+    def remove_comments(self, raw_string):
+        lines = raw_string.split('\n')
+        new_lines = []
+        block_comment = False
+        for line in lines:
+            new_line = self.remove_line_comment(line)
+            #get block comments
+            while any(['/*' in new_line, '*/' in new_line]):
+                if block_comment:
+                    if '*/' in new_line:
+                        new_line = new_line[new_line.index('*/') + 2:]
+                        print(new_line)
+                        block_comment = False
+                else:
+                    if '/*' in new_line:
+                        ni = new_line.index('/*')
+                        if ni:
+                            new_lines.append(new_line[:ni])
+                        new_line = new_line[ni + 2:]
+                        block_comment = True
+            if not block_comment:
+                if len(new_line):
+                    new_lines.append(new_line)
+        return new_lines
+
+    @staticmethod
+    def remove_line_comment(line):
+        if "'" in line:
+            new_line = line[:line.index("'")]
+            return new_line
+        else:
+            return line
